5c85ff9653429c7f7dd51cd4bcbf43482bb569c3
toonsevrin <twan123@live.be>
2020-05-01 20:41:05 +0200

    Fixed build tag location

--- api_integration_test.go
+++ api_integration_test.go
@@ -1,3 +1,5 @@
+//+build integration
+
 /*
  * MIT License
  *
@@ -21,8 +23,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
-//+build integration
 
 package businesscentral
 
